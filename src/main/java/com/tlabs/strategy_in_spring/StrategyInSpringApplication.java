e8bc9c066fa35944c8d2a2fae31fa3e6b7810485
Yuri <yurisokolenko@gmail.com>
2020-05-14 15:44:15 +0300

    name fixed

---
+++ src/main/java/com/tlabs/strategy_in_spring/StrategyInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tlabs.strategy_in_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyInSpringApplication.class, args);
+	}
+
+}
